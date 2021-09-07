d55c95b14ebe3da68a31af71f0b609d2f4840600
KhanTagiev <xan.2011@yandex.ru>
2021-09-07 15:54:14 +0300

    feat: init FlightSearchList, FlightCard.js jsx and style

--- src/components/FlightsSearch/FlightSearchList/FlightSearchList.js
+++ src/components/FlightsSearch/FlightSearchList/FlightSearchList.js
@@ -1,7 +1,45 @@
+import { useState, useEffect } from "react";
+
+import FlightCard from "./FlightCard/FlightCard";
 import "./FlightSearchList.css";
 
-function FlightSearchList() {
-  return <div className="search-list"></div>;
+function FlightSearchList({ flights }) {
+
+  const [filteredFlight, setFilteredFlights] = useState([]);
+  const [flightsCount, setFlightsCount] = useState(2);
+  const [flightsAddCount, setFlightsAddCount] = useState(0);
+
+  useEffect(() => {
+    if (flights.length > 0) {
+      setFilteredFlights(flights);
+      setFlightsAddCount(2);
+      console.log(flights[0]);
+    }
+  }, [flights]);
+
+  function onAddFlights() {
+    setFlightsCount(flightsCount + flightsAddCount);
+  }
+
+  return (
+    <div className="search-list">
+      <ul className="search-list__container">
+        {filteredFlight.length > 0 ? (filteredFlight.slice(0, flightsCount).map(item => <FlightCard key={item.flightToken} flight={item.flight}/>)) : <></>}
+      </ul>
+      <div className="search-list__btn-container">
+        <button
+            className={`search-list__btn ${
+                flightsCount >= flights.length ? "search-list__btn_hide" : ""
+            }`}
+            type="button"
+            aria-label="Кнопка"
+            onClick={onAddFlights}
+        >
+          Показать ещё
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default FlightSearchList;
--- src/components/FlightsSearch/FlightsSearch.js
+++ src/components/FlightsSearch/FlightsSearch.js
@@ -1,13 +1,22 @@
+import { useState, useEffect } from "react";
+
 import FlightSearchSidebar from "./FlightSearchSidebar/FlightSearchSidebar";
 import FlightSearchList from "./FlightSearchList/FlightSearchList";
 import "./FlightsSearch.css";
+import flightResult from "../../utils/flights.json";
 
 function FlightsSearch() {
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    setFlights(flightResult.result.flights);
+  }, []);
+
   return (
     <section className="section flight-search">
       <div className="flight-search__container">
         <FlightSearchSidebar />
-        <FlightSearchList />
+        <FlightSearchList flights={flights} />
       </div>
     </section>
   );
