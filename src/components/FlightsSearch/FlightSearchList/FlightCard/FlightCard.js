25026c5fd8f7c6326acb79d225be5a45f5911d50
KhanTagiev <xan.2011@yandex.ru>
2021-09-08 12:17:05 +0300

    feat: flightCard info view and styled

--- src/components/FlightsSearch/FlightSearchList/FlightCard/FlightCard.js
+++ src/components/FlightsSearch/FlightSearchList/FlightCard/FlightCard.js
@@ -2,14 +2,58 @@ import "./FlightCard.css";
 import carrierLogo from "../../../../images/carrier/airline.svg";
 
 function FlightCard({ flight }) {
+  function dateFormatter(str) {
+    const days = ["пн.", "вт.", "ср.", "чт.", "пт.", "сб.", "вс."];
+    const months = [
+      "янв.",
+      "фев.",
+      "мар.",
+      "апр.",
+      "мая",
+      "июня",
+      "июля",
+      "авг.",
+      "сен.",
+      "окт.",
+      "ноя.",
+      "дек.",
+    ];
+
+    const date = new Date(str);
+    const getDate = date.getDate();
+    const getMonth = date.getMonth();
+    const getDay = date.getDay();
+
+    return `${getDate} ${months[getMonth]} ${days[getDay - 1]}`;
+  }
+
+  function timeFormatter(str) {
+    const date = new Date(str);
+    const hours = String(date.getHours());
+    const minutes = String(date.getMinutes());
+
+    return `${hours.length === 1 ? `0${hours}` : hours}:${
+      minutes.length === 1 ? `0${minutes}` : minutes
+    }`;
+  }
+
+  function durationToTimeFormatter(num) {
+    const hours = Math.floor(num / 60);
+    const minutes = num % 60;
+    return `${hours} ч ${minutes} мин`;
+  }
+
   return (
     <article className="flight-card">
       <div className="flight-card__header">
-        <img
-          src={carrierLogo}
-          alt={flight.carrier.caption}
-          className="flight-card__carrier-logo"
-        />
+        <div className="flight-card__carrier-container">
+          <img
+            src={carrierLogo}
+            alt={flight.carrier.caption}
+            className="flight-card__carrier-logo"
+          />
+          <p className="flight-card__carrier-text">{flight.carrier.caption}</p>
+        </div>
         <div className="flight-card__price">
           <p className="flight-card__price-count">
             {flight.price.total.amount} &#8381;
@@ -23,25 +67,53 @@ function FlightCard({ flight }) {
         {flight.legs.map((leg) => (
           <li className="flight-card__route-container">
             <div className="flight-card__route-title">
-              <p>
+              <p className="flight-card__text">
                 {leg.segments[0].departureCity.caption},{" "}
-                {leg.segments[0].departureAirport.caption}{" "}
-                <span>({leg.segments[0].departureAirport.uid})</span>
+                {leg.segments[0].departureAirport.caption}
+                <span className="flight-card__text_blue">
+                  ({leg.segments[0].departureAirport.uid})
+                </span>
+                <span className="flight-card__text_blue">&rarr;</span>
+                {
+                  leg.segments[leg.segments.length - 1].arrivalCity.caption
+                },{" "}
+                {leg.segments[leg.segments.length - 1].arrivalAirport.caption}
+                <span className="flight-card__text_blue">
+                  ({leg.segments[leg.segments.length - 1].arrivalCity.uid})
+                </span>
               </p>
-              {/*<p>{leg.segments[1].departureCity.caption}, {leg.segments[1].departureAirport.caption} <span>({leg.segments[1].departureAirport.uid})</span></p>*/}
             </div>
             <div className="flight-card__route-dates">
-              <p>
-                20:40 <span>18 авг. вт</span>
+              <p className="flight-card__text">
+                {timeFormatter(leg.segments[0].departureDate)}
+                <span className="flight-card__text_blue">
+                  {dateFormatter(leg.segments[0].departureDate)}
+                </span>
               </p>
-              <p>◴ 14 ч 25 мин</p>
-              <p>
-                <span>19 авг. ср</span> 09:25
+
+              <p className="flight-card__text">
+                &#128337;{" "}
+                {durationToTimeFormatter(leg.segments[0].travelDuration)}
+              </p>
+              <p className="flight-card__text">
+                <span className="flight-card__text_blue">
+                  {dateFormatter(
+                    leg.segments[leg.segments.length - 1].arrivalDate
+                  )}
+                </span>
+                {timeFormatter(
+                  leg.segments[leg.segments.length - 1].arrivalDate
+                )}
+              </p>
+              {leg.segments.length > 1 ? (
+                <p className="flight-card__transfer-info">1 пересадка</p>
+              ) : null}
+            </div>
+            <div className="flight-card__route-carrier">
+              <p className="flight-card__text">
+                Рейс выполняет: {flight.carrier.caption}
               </p>
             </div>
-            <p className="flight-card__route-carrier">
-              Рейс выполняет: {flight.carrier.caption}
-            </p>
           </li>
         ))}
       </ul>
@@ -49,7 +121,11 @@ function FlightCard({ flight }) {
         className={`flight-card__btn`}
         type="button"
         aria-label="Кнопка"
-        onClick={() => alert("Вы выбрали рейс")}
+        onClick={() =>
+          alert(
+            `Вы выбрали рейс от ${flight.carrier.caption} стоимостью ${flight.price.total.amount} рублей`
+          )
+        }
       >
         Выбрать
       </button>
